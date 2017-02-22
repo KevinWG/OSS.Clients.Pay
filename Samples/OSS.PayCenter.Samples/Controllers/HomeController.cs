e8772c7f4eed460f2c4f302158230c630d81b1f2
Kevin <1985088337@qq.com>
2017-02-23 01:07:46 +0800

    签入微信支付框架和示例站点

---
+++ Samples/OSS.PayCenter.Samples/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OSS.PayCenter.Samples.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
