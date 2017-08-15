8029752812283526f854a3f763b8a5ed1510d729
Kevin <1985088337@qq.com>
2017-08-15 21:16:12 +0800

    升级版本

---
+++ Samples/OSS.PaySdk.Samples/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OSS.PaySdk.Samples.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
